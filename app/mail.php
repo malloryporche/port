264a5864b4bcb127265e640fa1c380b12ebe12a8
Mallory Porche <malloryburke@gmail.com>
2015-02-05 21:42:35 -0800

    CONTACT: Refactored HTML

--- app/mail.php
+++ app/mail.php
@@ -1,10 +1,11 @@
 <?php  $user_name = $_POST['user_name'];
  $user_email = $_POST['user_email'];
+ $user_subj = $_POST['user_subj'];
  $message = $_POST['message'];
  $formcontent = "From: $name /n Message: $message";
  $recipient = "malloryburke@gmail.com";
  $subject = "From your Contact Form";
 $mailheader = "From: $user_email /r/n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+mail($recipient, $user_subj, $formcontent, $mailheader) or die("Error!");
 echo "Thank you!  Your response has been recorded.";
 ?>
